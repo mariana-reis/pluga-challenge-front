2b2bf03f38069b2bfc60b6e8088e23c70648bb2e
mariana-reis <mariana.reis@al.resilia.com.br>
2025-07-12 21:26:16 -0300

    chore(config): add Prettier, ESLint and PostCSS configuration files

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'next', 'prettier'],
+    plugins: ['unused-imports', 'react-hooks', 'react'],
+    rules: {
+      semi: ['error', 'never'],
+      quotes: [
+        'error',
+        'single',
+        {
+          allowTemplateLiterals: true
+        }
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index']
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true }
+        }
+      ],
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      '@next/next/no-html-link-for-pages': 'error',
+      '@next/next/no-img-element': 'warn'
+    }
+  })
+]
+
+export default eslintConfig
